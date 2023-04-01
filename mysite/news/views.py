22e5fd2ff2b198e867dc479bcccbff4549ea9bbf
Zhanat Uturova <526628@gmail.com>
2023-04-02 04:24:08 +0600

    Create index.html. Change view for homepage. Delete test page

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
 
 # Create your views here.
+# def index(request):
+#     news = News.objects.all()
+#     res = '<h1>Список новостей</h1>'
+#     for item in news:
+#         res += f'<div>\n<h3>{item.title}</h3>\n<p>{item.content}</p>\n</div>\n<hr>'
+#     return HttpResponse(res)
+
 def index(request):
-    # print(dir(request))
-    return HttpResponse('hello world')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
 
-
-def test(request):
-    # print(dir(request))
-    return HttpResponse('<h1>тестовая страница</h1>')
