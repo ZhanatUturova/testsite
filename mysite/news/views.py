ca03f6b40491472dfb1126a3367510a133c28001
Zhanat Uturova <526628@gmail.com>
2023-04-04 21:29:21 +0600

    Create category.html. Add urls and views for category page

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 # Create your views here.
 # def index(request):
@@ -15,9 +15,22 @@ from .models import News
 def index(request):
     # news = News.objects.order_by('-created_at')   # теперь сортировка прописана в моделях
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'Список новостей'
+        'title': 'Список новостей',
+        'categories': categories
     }
     return render(request, template_name='news/index.html', context=context)
 
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'categories': categories,
+        'category': category
+    }
+    return render(request, template_name='news/category.html', context=context)
