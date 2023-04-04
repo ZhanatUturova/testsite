61e265c4483b1ded1e8f3ff812287a1fb64b1f07
Zhanat Uturova <526628@gmail.com>
2023-04-05 03:36:44 +0600

    Create news_tags. Add tags into _sidebar

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -15,22 +15,18 @@ from .models import News, Category
 def index(request):
     # news = News.objects.order_by('-created_at')   # теперь сортировка прописана в моделях
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
-        'categories': categories
     }
     return render(request, template_name='news/index.html', context=context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category
     }
     return render(request, template_name='news/category.html', context=context)
