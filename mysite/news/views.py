5877f2b105b5a597b8649c521e3ed17840b63b5d
Zhanat Uturova <526628@gmail.com>
2023-04-07 04:55:13 +0600

    Add 2 another way to write form in html. Describe NewsForm and rewrite froms view

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News, Category
 from .forms import NewsForm
@@ -24,13 +24,17 @@ def get_category(request, category_id):
 
 
 def view_news(request, news_id):
-    news_item = get_list_or_404(News, pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {'news_item': news_item})
 
 
 def add_news(request):
     if request.method == 'POST':
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
