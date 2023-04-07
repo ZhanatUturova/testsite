815492952ce7f228b06d93c99ea904a893122ad4
Zhanat Uturova <526628@gmail.com>
2023-04-07 22:30:10 +0600

    Add custom validator for title in form

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,4 +1,8 @@
+import re
+
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import News
 
 
@@ -13,3 +17,9 @@ class NewsForm(forms.ModelForm):
             'category': forms.Select(attrs={'class': 'form-control'})
         }
 
+    # пользовтаельский валидатор должен начинаться со слова clean
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинваться с цифры')
+        return title
