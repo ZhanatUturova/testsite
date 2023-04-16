918ecb4dc5fb27fbaa1bd1fb91fb18051b28f7b5
Zhanat Uturova <526628@gmail.com>
2023-04-16 06:20:49 +0600

    Customise admin panel

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published', )
     list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
+    save_on_top = True      # добавит кнопки сохранения вверху страницы
 
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="80">')
+        else:
+            return 'Фото не установлено'    # по-умолчанию выводит '-'
+
+    get_photo.short_description = 'Миниатюра фото'
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
@@ -17,3 +28,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)    # порядок важен!
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Управление новостями'
+admin.site.site_header = 'Управление новостями'
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -11,6 +11,7 @@ class HomeNews(ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     # extra_context = {'title': 'Главная'}    # используется только для статичных данных
+    # queryset = News.objects.select_related('category')    # избавляет от лишних sql запросов
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
