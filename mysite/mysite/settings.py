1ad890f9732a258688a064dbaf6d735f9988cab6
Zhanat Uturova <526628@gmail.com>
2023-05-01 03:40:38 +0600

    Add multi-level menu implemented using the module

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from .secrets import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -40,8 +42,10 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'captcha',
+    'mptt',
     'debug_toolbar',
     'news.apps.NewsConfig',
+    'testapp.apps.TestappConfig',
 ]
 
 MIDDLEWARE = [
@@ -139,8 +143,8 @@ INTERNAL_IPS = [
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='minikids.kg@gmail.com'
-EMAIL_HOST_PASSWORD='hqhrpixjnejhchwk'
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 EMAIL_PORT=587
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
@@ -228,3 +232,5 @@ CACHES = {
         }
     }
 }
+
+MPTT_ADMIN_LEVEL_INDENT = 20
